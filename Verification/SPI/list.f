94aa2d4afc08ae218b2fa1f2b446044d30c61230
Ivy-End <suda.ivywk@gmail.com>
2023-04-25 09:57:55 +0800

    dcsim file list

--- Verification/SPI/list.f
+++ Verification/SPI/list.f
@@ -1,2 +1,3 @@
-../../Design/SPI/SPI.v
+/backup-tools/PDK/XMC/XMC55_HP/STD/xmc55ef_hp/std_lib_20181213/hvt_std_lib_9t_20181213/verilog/sc9_55lp_base_hvt.v
+../../Synthesis/SPI/dc/netlist/SPI.cg.gv
 ./tb_SPI.sv
